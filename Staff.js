89653dcc6f7cd86fc7d7f258228369c2e3bb7781
minhieukt <minhieukt@gmail.com>
2022-09-05 14:50:23 +0700

    add list circle

--- Staff.js
+++ Staff.js
@@ -1,8 +1,18 @@
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+    Gender[Gender["Other"] = 2] = "Other";
+})(Gender || (Gender = {}));
 var Staff = /** @class */ (function () {
-    function Staff(name, email, age) {
+    function Staff(name, gender, email, age, numberPhone, birthday) {
+        this.gender = Gender.Other;
         this.name = name;
+        this.gender = gender;
         this.email = email;
         this.age = age;
+        this.numberPhone = numberPhone;
+        this.birthday = birthday;
     }
     Staff.prototype.setName = function (name) {
         this.name = name;
@@ -24,28 +34,73 @@ var Staff = /** @class */ (function () {
     };
     return Staff;
 }());
-var staff = new Staff("as", "123@ddd", 45);
-// console.log(staff)
-staff.setAge(69);
-// staff.setEmail("XXX@mkasdasd")
-console.log(staff);
-var Pt = /** @class */ (function () {
-    function Pt(a, b, c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    Pt.prototype.nghiempt = function () {
-        var x1, x2;
-        var delta = this.b * this.b - 4 * this.a * this.c;
-        if (delta < 0)
-            return -1;
-        else if (delta == 0)
-            return -this.b / (2 * this.a);
-        else
-            return 2;
-    };
-    return Pt;
-}());
-var pt = new Pt(3, 4, -5);
-console.log(pt.nghiempt());
+var listStaff = [];
+listStaff.push(new Staff("aaa", Gender.Male, "12@21", 21, 02022, new Date("1990-09-19")));
+listStaff.push(new Staff("bbb", Gender.Female, "12@21", 22, 412, new Date("1992-01-13")));
+listStaff.push(new Staff("ccc", Gender.Other, "12@21", 24, 4234, new Date("1996-05-29")));
+function show(a) {
+    console.log(a);
+}
+listStaff.forEach(show);
+// function main() {
+//     let message: string;
+//     let total: number = 100;
+//     let isProduction = true;
+//     let prices: Array<number> = [120, 88, 60]; // string 60 to number
+//     let languages: string[] = ['vi', 'en-us'];
+//     let now = new Date();
+//     let unknown: any;
+//
+//     enum Direction {
+//         UP,
+//         DOWN,
+//         LEFT,
+//         RIGHT
+//     };
+//
+//     function log(msg: string): void {
+//         console.log(msg)
+//     }
+//
+//     interface IPost {
+//         id: string;
+//         title: string;
+//         body?: string;
+//     }
+//
+//     isProduction = false;
+//     unknown = Direction.UP;
+//     unknown = 'changed';
+//     let post: IPost; // const ??
+//     message = "50";   // number 50 to string
+//
+//     function getPost(postId: string): IPost {
+//         // do something to retrieve post
+//         return {
+//             id: postId,
+//             title: 'Post Title',
+//             body: 'Post Body',
+//             extra: 'data'
+//         } as IPost;
+//     }
+// }
+// class Circle {
+//     color: string
+//     radius: number
+//
+//     constructor(color: string, radius) {
+//         this.color = color
+//         this.radius = radius
+//     }
+// }
+//
+// let circleList: Circle[] = []
+// circleList.push(new Circle("red", 30))
+// circleList.push(new Circle("blue", 36))
+// circleList.push(new Circle("green", 2))
+//
+// function showCircle(circle: Circle) {
+//     console.log("hinh tron " + circle.radius + " mau sac " + circle.color)
+// }
+//
+// circleList.forEach(showCircle)
--- Staff.ts
+++ Staff.ts
@@ -1,12 +1,24 @@
+enum Gender {
+    Male,
+    Female,
+    Other
+}
+
 class Staff {
     name: string
+    gender: Gender = Gender.Other
     email: string
     age: number
+    numberPhone: number
+    birthday?: Date
 
-    constructor(name: string, email: string, age: number) {
+    constructor(name: string, gender: Gender, email: string, age: number, numberPhone: number, birthday: Date) {
         this.name = name
+        this.gender = gender
         this.email = email
         this.age = age
+        this.numberPhone = numberPhone
+        this.birthday = birthday
     }
 
     setName(name: string): void {
@@ -34,9 +46,81 @@ class Staff {
     }
 }
 
-let staff = new Staff("as", "123@ddd", 45)
-console.log(staff)
-staff.setAge(69)
-staff.setEmail("XXX@mkasdasd")
-console.log(staff)
+let listStaff: Staff[] = []
+listStaff.push(new Staff("aaa", Gender.Male, "12@21", 21, 02022, new Date("1990-09-19")))
+listStaff.push(new Staff("bbb", Gender.Female, "12@21", 22, 412, new Date("1992-01-13")))
+listStaff.push(new Staff("ccc", Gender.Other, "12@21", 24, 4234, new Date("1996-05-29")))
+
+function show(staff: Staff) {
+    console.log(staff)
+}
+
+listStaff.forEach(show)
+
+// function main() {
+//     let message: string;
+//     let total: number = 100;
+//     let isProduction = true;
+//     let prices: Array<number> = [120, 88, 60]; // string 60 to number
+//     let languages: string[] = ['vi', 'en-us'];
+//     let now = new Date();
+//     let unknown: any;
+//
+//     enum Direction {
+//         UP,
+//         DOWN,
+//         LEFT,
+//         RIGHT
+//     };
+//
+//     function log(msg: string): void {
+//         console.log(msg)
+//     }
+//
+//     interface IPost {
+//         id: string;
+//         title: string;
+//         body?: string;
+//     }
+//
+//     isProduction = false;
+//     unknown = Direction.UP;
+//     unknown = 'changed';
+//     let post: IPost; // const ??
+//     message = "50";   // number 50 to string
+//
+//     function getPost(postId: string): IPost {
+//         // do something to retrieve post
+//         return {
+//             id: postId,
+//             title: 'Post Title',
+//             body: 'Post Body',
+//             extra: 'data'
+//         } as IPost;
+//     }
+// }
+
+class Circle {
+    color: string
+    radius: number
+
+    constructor(color: string, radius) {
+        this.color = color
+        this.radius = radius
+    }
+}
+
+let circleList: Circle[] = []
+circleList.push(new Circle("red", 30))
+circleList.push(new Circle("blue", 36))
+circleList.push(new Circle("green", 2))
+
+function showCircle(circle: Circle) {
+    console.log("hinh tron " + circle.radius + " mau sac " + circle.color)
+}
+
+circleList.forEach(showCircle)
+
+
+
 
